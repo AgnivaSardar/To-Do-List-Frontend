b44579a5eceac40b8eb0da0632334baf31c20770
First-Repository <agniva.sardar@gmail.com>
2025-09-03 17:09:09 +0530

    updated the base url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api';  
+const BASE_URL = 'https://to-do-list-backend.up.railway.app'
+';  
 
 export const getTasks = () => axios.get(`${BASE_URL}/tasks`);
 
